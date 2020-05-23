5099a65f193a0a890c3015b930f9d56783d4c23d
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-05-23 05:06:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Write your solution here!
 
 const append = ['Milo', 'Otis', 'Garfield'];
-const prepend= ['Milo', 'Otis', 'Garfield'];;
-const removeLast= ['Milo', 'Otis', 'Garfield'];;
-const removeFirst;
+const prepend = ['Milo', 'Otis', 'Garfield'];
+const removeLast = ['Milo', 'Otis', 'Garfield'];
+const removeFirst = ['Milo', 'Otis', 'Garfield'];
+
+append.push 'Odie'
+
+
